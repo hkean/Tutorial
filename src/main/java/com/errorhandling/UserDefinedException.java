2fe2dbe7299273fdd48a358744c0169786c6bc24
HK <HK@NPTPM-0695.mimos.local>
2019-11-14 11:21:00 +0800

    add "throw new" to existing exception

--- src/main/java/com/errorhandling/UserDefinedException.java
+++ src/main/java/com/errorhandling/UserDefinedException.java
@@ -1,5 +1,10 @@
 package com.errorhandling;
 
+import java.util.HashSet;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+
 class abc extends Exception {
 	private static final long serialVersionUID = -7595615251392907376L;
 
@@ -11,12 +16,14 @@ class abc extends Exception {
 }
 
 public class UserDefinedException {
-	public static void main(String[] args) throws abc, ArithmeticException {
+	public static void main(String[] args) throws Exception {
 		int s = 0;
-		if (s == 0) {
+		if (s == 1) {
 			throw new abc("custom made exception");
 		} else if (s == 0) {
 			throw new ArithmeticException("custom made exception");
+		} else if (s == -1) {
+			throw new ConstraintViolationException(new HashSet<ConstraintViolation<FormatErrorMessageMain>>());
 		}
 	}
 }
